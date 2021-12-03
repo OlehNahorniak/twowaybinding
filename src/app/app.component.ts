386c5cba19a662e94e97d39f91405f869d5babff
olehnahorniak <oleh.nahorniak@gmail.com>
2021-12-03 21:06:22 +0200

    two-way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ export class AppComponent {
   title = 'twowaybinding';
 
   public pName ="Oleh Nahorniak";
+  public message = "";
 
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
@@ -9,7 +9,11 @@ export class ContactsComponent implements OnInit {
 
 
   @Input('parentData') public  parentData:any;
-  
+  @Output() public childEvent = new EventEmitter();
+
+  fireEvent(value:any){
+    this.childEvent.emit(value);
+  }
 
   constructor() { }
 
